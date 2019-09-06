4a9ff46e08274fbdb23781ef74a4346fdebf60a2
TimKopp <timkopp@gmail.com>
2019-09-06 07:24:35 -0400

    adding reflection supporty for dynamic ctor

--- CSCodeCompiler/Reflection/Program.cs
+++ CSCodeCompiler/Reflection/Program.cs
@@ -17,26 +17,40 @@ namespace CSCodeCompiler.Projects
     {
         public static void Run(string[] args)
         {
-            
-        //ARgum
 
-            Type type = Type.GetType("CSCodeCompiler.Strategies.IndexCompile");
-            ConstructorInfo ctor = type.GetConstructor(new[] { typeof(int), typeof(int), typeof(string) });
-            object obj = ctor.Invoke(new object[] { 1, 10, "[index]"});
+            //ARgum
 
-             Macro macro = new Macro();
-            List<IStrategy> strat = new List<IStrategy>();
-            strat.Add((IStrategy)obj);
+            Type[] ctorSignature = Reflection.GetCtorArgs(new object[] {1,2,"hello"});
+            return;
+            //Macro macro = new Macro();
+            //List<IStrategy> strat = new List<IStrategy>();
+            //strat.Add((IStrategy)obj);
             //strat.Add(new IndexCompile(1, 0, "[SORTORDER]")); 
             //strat.Add(new IndexCompile(1000, 0, "[indexpk]"));
-            macro.Execute(strat);
-            macro.Commit();
+            //macro.Execute(strat);
+            //macro.Commit();
 
 
             //MethodInfo methodInfo = type.GetMethod("DBLookup");
             //object val = methodInfo.Invoke(obj, new object[] { " SELECT CODE + '\n' From fsma_QuestionTypes " });
 
             //Console.WriteLine("{0}", val);
+        }
+        public static Type[] GetCtorArgs( object[] args) { 
+            Type[] ctorargs = new Type[5];
+            for (int i = 0; i < args.Length; i++)
+            {
+                ctorargs[i] = args[i].GetType();
+                Console.WriteLine("{0}", ctorargs[i]);
+            }   
+            return ctorargs;
+        }
+        public void InstantiateType() {
+            Type type = Type.GetType("CSCodeCompiler.Strategies.IndexCompile");
+            ConstructorInfo ctor = type.GetConstructor(new[] { typeof(int), typeof(int), typeof(string) });
+            object obj = ctor.Invoke(new object[] { 1, 10, "[index]" });
+            Console.WriteLine("{0}$1-10-[index]", obj.GetHashCode().ToString());
+            Console.Read();
         }
     }
 }
